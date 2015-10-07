c8d2c2fcecee791c177090be59c60c5df562d316
Andre Pitombeira <andrepitombeira@gmail.com>
2015-10-07 17:41:58 +0200

    Migrate project to rails 3.2.11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackbonerailsDemo::Application.config.secret_token = '179c9c8ac8180eb2aa512bf375f3349700e1ddf9de27b197e657bf8bdbafd5c0c7e7ab76cf21323d814e5e6f64ecdb11a818edf72811d096b311b0b5c6dbc0b4'
